7acc581f1d11fd75301e7d9ccf0215b403e22ffd
wael <dim.wael@gmail.com>
2020-05-17 14:11:21 +0100

    add flask and WebInterface

---
+++ app.py
@@ -0,0 +1,19 @@
+from pipeline import answer_question
+from flask import Flask, jsonify, render_template, request
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return render_template("main_page.html")
+
+
+@app.route('/question', methods=['GET', 'POST'])
+def ask_question():
+    question = request.json.get('input_question')
+    answer, best_paragraph, url = answer_question(question)
+    return jsonify({'link': url, 'text_paragraphs': best_paragraph, 'albert': answer})
+
+
+if __name__ == '__main__':
+    app.run()
